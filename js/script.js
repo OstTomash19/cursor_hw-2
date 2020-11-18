cf637b16ba8bfc23f5703a2dc2c36ecc6bb0f279
Ostap <o.tomash650416@gmail.com>
2020-11-18 17:12:15 +0200

    fixed version

--- js/script.js
+++ js/script.js
@@ -1,30 +1,23 @@
-let firstNumberForCount = prompt(`Вкажіть ціле число, з якого ми починатимемо скаладати числа`, ``); 
+let firstNumberForCount = +prompt(`Вкажіть ціле число, з якого ми починатимемо скаладати числа`, ``); 
 let sumOfNumbers = 0;
 
-function isFloat(value) {
-    return value % 1 !== 0;
+while(!Number.isInteger(firstNumberForCount) || !firstNumberForCount) {
+    firstNumberForCount = +prompt(`Вкажіть будь-ласка ціле число, з якого ми починатимемо підрахунок`, ``);
 }
-
-while(isNaN(firstNumberForCount) || isFloat(firstNumberForCount) || !firstNumberForCount) {
-    firstNumberForCount = prompt(`Вкажіть будь-ласка ціле число, з якого ми починатимемо підрахунок`, ``);
-}
-
-firstNumberForCount = Number(firstNumberForCount);
 
 let secondNumberForCount = +prompt(`Вкажіть ціле число, до якого ми будемо складати числа (включно)`, ``); 
 
-while(isNaN(secondNumberForCount) || isFloat(secondNumberForCount) || firstNumberForCount >= secondNumberForCount) {
+while(!Number.isInteger(secondNumberForCount) || firstNumberForCount >= secondNumberForCount) {
     secondNumberForCount = +prompt(`Вкажіть будь-ласка ціле число, до якого ми будемо складати числа (включно)`, ``);
 }
 
 const skipEvenNumbers = confirm(`Пропускати парні числа при додаванні?`);
 
 for(let i = firstNumberForCount; i <= secondNumberForCount; i++) {
-    if(!skipEvenNumbers) {
+    if(skipEvenNumbers && i % 2 === 0) {
+        continue;
+    } 
         sumOfNumbers += i;
-    } else {
-        i % 2 === 0 ? 0 : sumOfNumbers += i;
-    }
 }
 
 console.log(`Сума цифр дорівнює : ${sumOfNumbers};`);
